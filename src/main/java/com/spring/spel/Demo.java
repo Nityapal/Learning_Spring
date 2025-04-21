9d1f774f7d6aeac2da8d773f6b93883336e75bb1
Nitya <22bch036@nith.ac.in>
2025-04-21 21:43:46 +0530

    how to call static methods,variables and how to create objects in expression

--- src/main/java/com/spring/spel/Demo.java
+++ src/main/java/com/spring/spel/Demo.java
@@ -14,6 +14,26 @@ public class Demo {
 	
 	@Value("#{T(java.lang.Math).sqrt(25)}")
 	private int z;
+	
+	@Value("#{T(java.lang.Math).PI}")
+	private double e;
+	
+	@Value("#{new java.lang.String('nitya')}")
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	
 	public int getZ() {
 		return z;
 	}
@@ -34,7 +54,7 @@ public class Demo {
 	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + ", z=" + z + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
 	}
 	
 	
